64f1662fd549467cd51e1a5f96cf2e9d5cd56252
Dragon3DGraff <dragon3dgraff@mail.ru>
2020-11-10 01:49:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+cookieParser = require('cookie-parser');
+const path = require('path');
+
+const app = express();
+app.use(cookieParser());
+app.use(express.json({ extended: true }));
+app.use('/api/check', require('./routes/checkAuth.routes'));
+app.use('/api/auth', require('./routes/auth.routes'));
+
+const PORT = config.get('port');
+
+if (process.env.NODE_ENV === 'production') {
+	app.use('/', express.static(path.join(__dirname, 'client')))
+  
+	app.get('*', (req, res) => {
+	  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+	})
+  }
+
+async function start() {
+	try {
+		await mongoose.connect(config.get('mongoURI'), {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+			})
+		app.listen(PORT, () => console.log(`Server has been started at ${PORT}`))
+	} catch (error) {
+		console.log('Server error', error.message);
+		process.exit(1);
+	}
+}
+
+start()
---
+++ routes/auth.routes.js
@@ -0,0 +1,99 @@
+const { Router } = require('express');
+const router = Router();
+const config = require('config');
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const {check, validationResult} = require('express-validator');
+cookieParser = require('cookie-parser');
+
+router.post('/register',
+	[
+		check('name', 'Enter name').exists(),
+		check('email', "Incorrect email").isEmail(),
+		check('password', 'Min password length is 6 symbols').isLength({ min: 6 })
+	],
+	async (req, res) => {
+	try {
+		const errors = validationResult(req);
+		if(!errors.isEmpty()){
+			return res.status(400).json({
+				errors: errors.array(),
+				 message: 'Incorrect registration data'
+			})
+		}
+		const {name, email, password} = req.body;
+		const candidate = await User.findOne({ email })
+
+		if (candidate) {
+			return res.status(400).json({ message: 'User is already exist' })
+		}
+
+		const hashedPassword = await bcrypt.hash(password, 12);
+		const user = new User({ name, email, password: hashedPassword });
+		await user.save();
+		res.status(201).json({ message: 'Account created!' });
+	} catch (error) {
+		res.status(500).json({ message: "ERROR"})
+		console.log(error);
+	}
+})
+
+router.post(
+	'/login',
+	[
+		check('email', "Incorrect email").normalizeEmail().isEmail(),
+		check('password', 'Enter password').exists()
+	],
+	 async (req, res) => {
+		 try {
+			const errors = validationResult(req);
+			if(!errors.isEmpty()){
+				return res.status(400).json({
+					errors: errors.array(),
+					 message: 'Incorrect registration data'
+				})
+			}
+			const {email, password} = req.body;
+			const user = await User.findOne({email});
+
+			if(!user){
+				return res.status(400).json({message: 'User not found'});
+			}
+
+			const isMatch = await bcrypt.compare(password, user.password);
+
+			if(!isMatch){
+				return res.status(400).json({message: 'Wrong password'});
+			}
+
+			const token = jwt.sign(
+				{userId: user.id},
+				config.get('jwtSecret'),
+				{expiresIn: '1h'}
+			)
+
+			res.clearCookie('token');
+			res.cookie('token', token, { httpOnly: true });
+
+			res.json( { userId: user.id, userName: user.name });
+			 
+		 } catch (error) {
+			res.status(500).json({ message: "ERROR"})
+			console.log(error);
+			 
+		 }
+})
+
+router.post(
+	'/logout',
+	 async (req, res) => {
+		 try {
+			res.clearCookie('token');
+			res.json( { userId: undefined, userName: undefined });
+		} catch (error) {
+			res.status(500).json({ message: "ERROR"})
+		}
+})
+
+module.exports = router;
