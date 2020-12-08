0d0f819dfbeefcf79b3b2e23204c63a752eb90fe
Dragon3DGraff <dragon3dgraff@mail.ru>
2020-12-08 23:53:49 +0300

    formatted with Prettier

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const express = require('express');
-const config = require('config');
-const mongoose = require('mongoose');
-cookieParser = require('cookie-parser');
-const path = require('path');
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json({ extended: true }));
-app.use('/api/check', require('./routes/checkAuth.routes'));
-app.use('/api/auth', require('./routes/auth.routes'));
+app.use("/api/check", require("./routes/checkAuth.routes"));
+app.use("/api/auth", require("./routes/auth.routes"));
 
-const PORT = config.get('port');
+const PORT = config.get("port");
 
-if (process.env.NODE_ENV === 'production') {
-	app.use('/', express.static(path.join(__dirname, 'client')))
-  
-	app.get('*', (req, res) => {
-	  res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
-	})
-  }
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static(path.join(__dirname, "client")));
 
-async function start() {
-	try {
-		await mongoose.connect(config.get('mongoURI'), {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true
-			})
-		app.listen(PORT, () => console.log(`Server has been started at ${PORT}`))
-	} catch (error) {
-		console.log('Server error', error.message);
-		process.exit(1);
-	}
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "index.html"));
+  });
 }
 
-start()
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoURI"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    app.listen(PORT, () => console.log(`Server has been started at ${PORT}`));
+  } catch (error) {
+    console.log("Server error", error.message);
+    process.exit(1);
+  }
+}
+
+start();
--- models/User.js
+++ models/User.js
@@ -1,20 +1,20 @@
-const { model, Schema } = require('mongoose');
+const { model, Schema } = require("mongoose");
 
 const schema = new Schema({
-	name: {
-		type: String
-	},
-	email: {
-		type: String,
-		required: true,
-		unique: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	registered: { type: Date, default: Date.now },
-	role: {type: String}
-})
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  registered: { type: Date, default: Date.now },
+  role: { type: String },
+});
 
 module.exports = model("User", schema);
